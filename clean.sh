06c7cd8764e8667e7bb2eb29c1aa5c70cbbf84ba
Samuel Coelho <samuelfcmc@gmail.com>
2015-08-18 16:35:38 +0100

    Changed not numbered chapters to numbered ones

--- clean.sh
+++ clean.sh
@@ -1,4 +1,14 @@
 FILES="*.aux *.bb *.bl *.lo *.lo *.nl *.ou *.to *.bc *.run.xml *.bbl *.blg *.fdb_latexmk *.fls *.log *.toc *.synctex.gz"
 rm -f $FILES
+cd abstract
+rm -f $FILES
+cd ..
+cd acknowledgments
+rm -f $FILES
+cd ..
+cd bibliography
+rm -f $FILES
+cd ..
 cd chapters
 rm -f $FILES
+cd ..
